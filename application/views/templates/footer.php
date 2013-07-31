82f5759d75ca92964a2cc3e0999a1ec6f1b0e72a
WebDevJL <webdev.jl@gmail.com>
2013-07-31 22:03:01 +0100

    footer page update

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,9 +1,9 @@
-    <script src="Scripts/jquery-1.9.1.js"></script>
-    <script src="Scripts/knockout-2.2.1.js"></script>
-    <script src="Scripts/toastr.js"></script>
-    <script src="Scripts/ui-dynamic-css.js"></script>
-    <script src="Scripts/bootstrap.js"></script>
-    <script src="Scripts/sammy-0.7.4.js"></script>
+    <script src="scripts/jquery-1.9.1.js"></script>
+    <script src="scripts/knockout-2.2.1.js"></script>
+    <script src="scripts/toastr.js"></script>
+    <script src="scripts/ui-dynamic-css.js"></script>
+    <script src="scripts/bootstrap.js"></script>
+    <script src="scripts/sammy-0.7.4.js"></script>
     <script src="AppJS/durandal/amd/require.js" data-main="AppJS/main.js"></script>
 </body>
 </html>
