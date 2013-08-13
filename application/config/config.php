8c1b2ef200c72b1aa849724dfd7d589ce061b39f
WebDevJL <webdev.jl@gmail.com>
2013-08-13 20:57:20 +0100

    update: timeout session back to 7200sec

--- application/config/config.php
+++ application/config/config.php
@@ -245,7 +245,7 @@ $config['encryption_key'] = 'cd307d03dd329a42cec6a928405b0d526ea7a030';
 |
 */
 $config['sess_cookie_name']		= 'sbID';
-$config['sess_expiration']		= 60;
+$config['sess_expiration']		= 7200;
 $config['sess_expire_on_close']	= FALSE;
 $config['sess_encrypt_cookie']	= FALSE;
 $config['sess_use_database']	= FALSE;
